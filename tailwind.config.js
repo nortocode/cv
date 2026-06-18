a5ae1fc3d537dd4618c424a9df5d6bfc09818e74
Name <user@example.com>
2026-06-18 14:41:47 +0200

    Migradas variables de colores a tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: 'var(--bg)',
+        text: 'var(--text)',
+        card: 'var(--card-bg)'
+      }
+    },
   },
   plugins: [],
 }
